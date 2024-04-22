364412ec41b7361e0732378f95b46a1ff9e0da07
Darshposhiya29 <112972676+Darshposhiya29@users.noreply.github.com>
2024-04-22 23:07:25 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,7 +8,7 @@ export const Login = (props) => {
 
     const handleSubmit = async (e) => {
         e.preventDefault()
-        const response = await fetch('http://localhost:5000https://backend-hazel-two.vercel.app/api/auth/login', {
+        const response = await fetch('https://backend-hazel-two.vercel.app/api/auth/login', {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
